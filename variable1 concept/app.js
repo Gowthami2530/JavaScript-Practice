fffe353c00949e746de15dab403e02f746a0a18b
Gowthami2530 <akulagowthami25@gmail.com>
2023-10-26 11:02:38 +0530

    app code

---
+++ variable1 concept/app.js
@@ -0,0 +1,14 @@
+var uid = "jabc123"
+var fullName = "Gowthami";
+var email = "gowthami123@gmail.com";
+var password = "12345";
+var confirmPassword = "12345";
+var courseCount = 0;
+var isLoggedInFromGoggle = false;
+
+console.log(`
+    With Unique Id: ${uid}
+    User name : ${fullName}
+    User email : ${email}
+    User password : ${password}
+`);
