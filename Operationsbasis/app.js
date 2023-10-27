6b7c9c83ebef12faacc5a6daaa5d6506d58090eb
Gowthami2530 <akulagowthami25@gmail.com>
2023-10-27 12:36:27 +0530

    app code

---
+++ Operationsbasis/app.js
@@ -0,0 +1,9 @@
+// + - % > < /
+var num1 = 7;
+var num2 = 6;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1%num2);
+console.log(num1/num2);
+console.log(num1>num2);
+console.log(num1<num2);
