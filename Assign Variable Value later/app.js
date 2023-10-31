c258b61eb5e723ce7aff2b508c1e296e02debcff
Gowthami2530 <akulagowthami25@gmail.com>
2023-10-31 20:39:36 +0530

    app code

---
+++ Assign Variable Value later/app.js
@@ -0,0 +1,13 @@
+// variables - Most basics building block
+// variables  - store, access, modify === value
+// Declare, assigment operator, assign value
+// Assign value later , Modify existing
+
+let firstName = "This is Gowthami";
+let address , zip, state;
+firstName = "Gowthami Akula";
+address = "Vijayawada";
+zip = 521212;
+state = "AP";
+console.log(address,zip,state);
+console.log(firstName);
