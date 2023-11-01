394c2718bb526165b01e86d6e78ed56c29521acc
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-01 12:02:02 +0530

    app code

---
+++ Variable Naming Rules/app.js
@@ -0,0 +1,39 @@
+// Variable Naming Rules 
+// can contain digits , letters, underscore,$
+// Must start with letter,$ or _
+// no keyword should be taken as variable name
+// cannot start with number as the variable name
+// case sensitive - fullname vs FullName
+// camelCase or underscore
+
+let random_1234_$ = "Gowthami";
+console.log(random_1234_$);
+
+let _$random_1234_$ = "Gowthami";
+console.log(_$random_1234_$);
+
+// if we keep number then it will give error
+// let 1_$random_1234_$ = "Gowthami";
+// console.log(1_$random_1234_$);
+
+// if we use the keyword as the variable name then it give the error
+
+// let let = "Gowthami";
+// console.log(let);
+
+// javascript is Case sensitive what ever we have declare as the variable name same we should declare in the console log 
+// let firstname = "Gowthami";
+// console.log(firstName);
+
+let firstname = "Gowthami";
+console.log(firstname);
+
+// camelCase means the first word firstletter should be small and the second word of the firstletter should be capital
+
+let firstName = "Gowthami";
+console.log(firstName);
+
+// underscore
+
+let first_name = "Gowthami";
+console.log(first_name);
