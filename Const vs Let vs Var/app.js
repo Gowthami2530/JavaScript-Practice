459e34ce283020ea55ab2071298ea43978b98e17
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-02 20:25:55 +0530

    app code

---
+++ Const vs Let vs Var/app.js
@@ -0,0 +1,19 @@
+//  CONST vs LET vs VAR 
+
+// Using Var
+var someValue = "some data";
+someValue = "new value added for var";
+
+// Using let 
+let firstName;
+firstName = "GowthamiAkula";
+
+// Using Const - we cannot re-assing the values
+const address = "AP";
+// const address;
+// address="AP";
+
+
+console.log(someValue);
+console.log(firstName);
+console.log(address);
