b7f7013d81eb60029b46415bdd35b15e0f4d9748
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-03 20:45:36 +0530

    app code

---
+++ Quotationmarks/app.js
@@ -0,0 +1,25 @@
+// Quotation Marks " " or ''
+
+const firstName = "Gowthami";
+console.log(firstName);
+
+const firstName1 = 'Gowthami';
+console.log(firstName1);
+
+// if we write like this ('Gowthami's') we will get the error up to gowthami only they have gave the string at last if we keep the Quotation
+// const firstName3 = 'Gowthami's have HTML, CSS, JavaScript';
+// console.log(firstName3);
+
+// if we want to print the whole sentence then we need to keep the \(backword slash) before the quotation 
+const firstName3 = 'Gowthami\'s have HTML, CSS, JavaScript';
+console.log(firstName3);
+
+// by using this ""
+const firstName4 = "Gowthami's have HTML, CSS, JavaScript";
+console.log(firstName4);
+
+
+// we should not use like this we will get error
+// const firstName2 = "Gowthami';
+// console.log(firstName2);
+
