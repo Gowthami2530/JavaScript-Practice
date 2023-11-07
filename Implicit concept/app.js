d1246827c7ad9497334d6f247692945e870b44d3
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-07 11:20:36 +0530

    app code

---
+++ Implicit concept/app.js
@@ -0,0 +1,44 @@
+//Implicit concept
+// Implicit is a Type Converter
+
+// Now we are giving the number in string and this Implicit will convert the string into number and it will give the result
+
+let number1 = "10";
+let number2 = "20";
+let result = number1 - number2;
+console.log(result);
+
+// Now we have gave a number 
+let number3 = 10;
+let number4 = 20;
+let result2 = number3 - number4;
+console.log(result);
+
+let number5 = 10;
+let number6 = 20;
+let result3 = number5 + number6;
+console.log(result3);
+
+
+// Now we will give the string and if we give + opeartion for the string it concatenation string 
+
+let firstName = "Gowthami";
+let secondName = "Akula";
+let fullName = firstName +" "+secondName;
+console.log(fullName);
+
+// if we give the number as a string and if we give the + operation for the number and it will take the 
+// number as a string and it concatenation the number and it will give the result
+let number7 = "10";
+let number8 = "20";
+let result4 = number7 + number8;
+console.log(result4);
+
+// Now we will give the string and for those string we will give the mathematicl operation and it will not work for the string
+// and it will give the result as NaN (NaN means it Not-a-Number)
+let first = "gowthami";
+let second = "akula";
+let result1 = first - second;
+console.log(result1);
+
+ 
