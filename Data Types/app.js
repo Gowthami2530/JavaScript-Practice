e25540f6189cbaaa5d8dc4228d3dfb064090b313
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-08 20:23:41 +0530

    app code

---
+++ Data Types/app.js
@@ -0,0 +1,33 @@
+// Data Types
+// Primitive - String, Number,Boolean,Null,Undefined,Sumbol.
+// Object - Arrays, Functions, Objects.
+
+// typeof - operator (typeof variable) or (typeof value)
+
+// string
+const firstName = "Gowthami";
+// Number
+const number = 100;
+
+// Boolean
+const value1 = true;
+const value2 = false;
+
+// Null
+const name = null;
+
+// undefined
+let lastName;
+
+
+// symbol(ES6)
+
+console.log(typeof firstName);
+console.log(typeof "hello");
+console.log(typeof number );
+console.log(typeof value1 );
+console.log(typeof value2 );
+console.log(typeof name );
+console.log(typeof lastName );
+
+
