852a12218b7355daeff2f4db52b8df9e302213d5
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-09 11:49:56 +0530

    app code

---
+++ Array Concept/app.js
@@ -0,0 +1,24 @@
+//Aray comcept
+// Array can be declared in [] bracket 
+// Array are having index values start with 0
+
+let fruit = "apple";
+let fruit1 = "Mango";
+let fruit2 = "banana";
+console.log(fruit);
+
+// Now we declared in array
+
+let fruits = [25,"apple","mango","banana",5,5,4];
+console.log(fruits);
+
+// we can select the particular element in array 
+
+console.log(fruits[0]);
+// if we give the index value that we did not define in the array we will get the result as 
+console.log(fruits[8]);
+
+const fruit4 = [25,"apple","mango","banana",5,5,4];
+console.log(fruit4[5]);
+fruit4[5] = "Grapes";
+console.log(fruit4);
