1a663a3cc933b92c2be81875fc2aae5488c981f2
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-16 13:08:22 +0530

    app code

---
+++ Conditional (if)statement/app.js
@@ -0,0 +1,38 @@
+// conditional (if) statement
+// >,<,>=,<=,==,===,!=,!==
+
+let number1 = 7;
+let number2 = 5;
+
+if(number1>number2){
+    console.log("Yes number1 is greater than number2");
+}else{
+    console.log("No number1 is not greater that number2");
+}
+
+if(number1<number2){
+    console.log("Yes number1 is greater than number2");
+}else{
+    console.log("No number1 is not greater than number2");
+}
+
+if(number1==number2){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
+
+// === will check the those two are equall are not and it will check the typeof of those 
+
+let value1 = "7";
+let value2 = 7;
+
+if(value1===value2){
+    console.log("Yes it is a having equal values and same type of data");
+}else{
+    console.log("No it is not having the same datatype");
+
+}
+
+
+
