f74e820fa88264608e9c71bcf2d9d08ded8cc818
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-18 13:13:04 +0530

    app code

---
+++ Equality concept/app.js
@@ -0,0 +1,29 @@
+// Equality concept
+// Conditional statements
+// >,<,>=,<=,==,===,!=,!==
+// == Checks only Value
+// === checks value and type
+
+const num1 =10;
+const num2  = 10;
+
+
+const value1 = num1==num2;
+console.log(value1);
+ 
+const value2 = num1===num2;
+console.log(value2);
+
+const value3 = num1!=num2;
+console.log(value3);
+
+// it will check the the type 
+const value4 = num1!==num2;
+console.log(value4);
+
+
+const num3 = 20;
+const num4 = "20";
+
+const value5 = num3===num4;
+console.log(value5);
