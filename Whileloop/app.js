31b7e8c19d042839adde43f93f84038e45249b92
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-23 19:26:50 +0530

    app code

---
+++ Whileloop/app.js
@@ -0,0 +1,11 @@
+// while loop concept
+// while loop will be used when we dont now how many times we need to execute the loop then we will use the while loop
+// while loop is a infinite loop
+// Repeate a code when the while condition is true
+
+let num = 10;
+
+while (num> 0) {
+    console.log("Execute this statement "+num);
+    num--;
+}
