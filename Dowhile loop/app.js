7cc97aaa472b469693d8a3596c5e8b536a37ef36
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-24 11:35:14 +0530

    app code

---
+++ Dowhile loop/app.js
@@ -0,0 +1,15 @@
+// Dowhile loop concept
+// Dowhile loop will run atleast once if it condition fail also 
+// do will execute first and then it will check the while condition 
+// syntax for dowhileloop is 
+// do{    
+// }
+// while()
+ 
+let amount = 5;
+
+do{
+    console.log("You have amount:"+amount);
+    amount++;
+}
+while (amount<10);
