2125fc21984f65af902b4fa1c075f8f3722f993b
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-25 11:13:18 +0530

    app code

---
+++ Forloop/app.js
@@ -0,0 +1,10 @@
+// for loop concept
+// for loop will be used when we know how many time we need  to execute the code then we use the for loop 
+// in for loop first we declare the variable,condition, increment/decrement
+// for(Initilization,condition,Increment/decrement)
+
+let i;
+
+for(i=0;i<=10;i++){
+    console.log("The number is:"+i);
+}
