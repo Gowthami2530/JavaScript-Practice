b868fab7e7c907e8068e9e66ac69e9639d7969e5
Gowthami2530 <akulagowthami25@gmail.com>
2023-11-28 12:30:06 +0530

    app code

---
+++ Template literals/app.js
@@ -0,0 +1,16 @@
+// Template Literals - ES6+
+// Backticks Characters `` --> Above the tab button
+// Interpolation ${} - Insert Expression(Values)
+
+const firstName = "Gowthami";
+const age = 23;
+
+const sentence = "She is" +" " + firstName + " " +  "she is"+ " " + age + " "+ "Years old";
+console.log(sentence);
+
+// by using the Backticks
+
+const firstName1 = "JayanthBabu";
+const age1 = 22;
+const value = `hey its ${firstName1} he is ${age} years old`;
+console.log(value);
