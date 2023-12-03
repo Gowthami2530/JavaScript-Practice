eb2efc2b65bbf5da9145f3cf553f0dacadc47beb
Gowthami2530 <akulagowthami25@gmail.com>
2023-12-03 20:15:45 +0530

    app code

---
+++ Null vs Undefined/app.js
@@ -0,0 +1,20 @@
+// Null vs Undefined
+// Both represent no value
+
+// Null - null will be  defined by the developer in the program
+// Undefined - "javaScript can not defined any value to this."
+// Variable without the value
+// Missing function arguments
+// Missing object properties
+
+let number1 = 20 + null;  // 20 + 0 
+console.log(number1);
+
+let name1 = null;
+console.log(name1);
+
+let name2 = 30 + undefined
+console.log(name2);
+
+let number2; // there we have declared the variable and to that variable we didnot assigin the value so we get the result as undefine
+console.log(number2);
