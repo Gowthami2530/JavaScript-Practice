d80f4793dcffd348552a580f5f39aa2d2235e22a
Gowthami2530 <akulagowthami25@gmail.com>
2023-12-05 20:36:19 +0530

    app code

---
+++ ternary operater/app.js
@@ -0,0 +1,24 @@
+// unary operater - typeof
+let num = "Gowthami";
+console.log(typeof num);
+// binary operater - assigment 
+let num1 = 2 + 5;
+console.log(num1);
+// ternary operater 
+// condition ? (run if it is true) : (run if it is false)
+
+let text = "Gowthami akula";
+
+if(text){
+    console.log(`hey it is have text it is true ${text}`);
+}else{
+    console.log("Its is not having the text so it is falsy value");
+}
+
+// now by using the ternary operater we can write the code like this the ternary operater are ? and : 
+
+let text1 = "Akula gowthami";
+text1 ? console.log(`hey it is have text it is true ${text1}`) : console.log("Its is not having the text so it is falsy value") ;
+
+let text2 = "";
+text2 ? console.log(`hey it is have text it is true ${text2}`) : console.log("Its is not having the text so it is falsy value") ;
