bbb8868f60b19dbc1dc94926979aa2d612434d0c
Gowthami2530 <akulagowthami25@gmail.com>
2023-12-10 19:59:00 +0530

    app code

---
+++ Array Iteration method/app.js
@@ -0,0 +1,13 @@
+// Array Iteration method
+// Powerfull Array Method
+// Array Method - forEach, map, filter, reduce
+// Iterate over array - for loop is not requied
+// Accept Callback function as an argument, calls callback
+// against each item in a array. Reference Item in the callback parameter.
+
+
+const num1 = [1,2,3,4,5];
+
+for(let i=0; i<num1.length; i++){
+  console.log(num1[i]);
+}
