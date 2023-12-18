4bb51ba962dced5f34da5a27126cfd4edfdca0d4
Gowthami2530 <akulagowthami25@gmail.com>
2023-12-18 20:35:59 +0530

    app code

---
+++ Variable concept/app.js
@@ -0,0 +1,8 @@
+var fullName = "Gowthami";
+var courseName = "React JS Bootcamp";
+var isLoggedIn = false;
+var  loggedCount = 34;
+console.log(loggedCount);
+var paymentMode;
+paymentMode = "Credit Card";
+console.log(paymentMode);
