ebef1f6d70bdff7ed05f114af40fb220e984b2f7
Gowthami2530 <akulagowthami25@gmail.com>
2023-12-20 13:09:51 +0530

    app code

---
+++ JavaScript DOM Introduction/app.js
@@ -0,0 +1,6 @@
+console.log("javaScript DOM Introduction");
+alert("Welcome to javaScript DOM Introduction");
+// if write the like this also it will give the same result when we refresh the page then it gives the popup message
+// window.alert("Welcome to javaScript DOM Introduction");
+
+// console.log(document);
