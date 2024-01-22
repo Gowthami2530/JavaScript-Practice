13713a9985c66786c01429bb31d49a45fa6d230a
Gowthami2530 <akulagowthami25@gmail.com>
2024-01-22 12:59:31 +0530

    app code

---
+++ javascript styling using DOM/app.js
@@ -0,0 +1,6 @@
+// javascript styling using DOM 
+
+const navbar = document.querySelector(".navbar");
+navbar.style.backgroundColor = "red";
+navbar.style.color="white";
+navbar.style.fontSize = "20px";
